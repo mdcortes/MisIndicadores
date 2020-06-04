1fc899131c32decfe2826213ab8561b954ef0f3e
Mario Cortes <m.cortesleon@uandresbello.edu>
2020-06-04 03:55:02 -0400

    Added base AndroidX Navigation component with a template login fragment

---
+++ app/src/main/java/com/prueba/misindicadores/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.prueba.misindicadores.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
