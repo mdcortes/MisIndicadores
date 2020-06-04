d090aa6e5ccc2d3c307ed00d8b2310a2118a211b
Mario Cortes <m.cortesleon@uandresbello.edu>
2020-06-04 03:02:42 -0400

    Initial commit/Base project

---
+++ app/src/main/java/com/prueba/misindicadores/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.prueba.misindicadores
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
