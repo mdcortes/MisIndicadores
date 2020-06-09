e6d5e88b4c20781a747fb5f610773695e8b89689
Mario Cortes <m.cortesleon@uandresbello.edu>
2020-06-09 01:03:25 -0400

    Base Indicator fragment (from Android Studio template) added

---
+++ app/src/main/java/com/prueba/misindicadores/ui/indicators/IndicatorDetailFragment.kt
@@ -0,0 +1,32 @@
+package com.prueba.misindicadores.ui.indicators
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.prueba.misindicadores.R
+
+class IndicatorDetailFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = IndicatorDetailFragment()
+    }
+
+    private lateinit var viewModel: IndicatorDetailViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_indicator_detail, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(IndicatorDetailViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/prueba/misindicadores/ui/indicators/IndicatorDetailViewModel.kt
@@ -0,0 +1,7 @@
+package com.prueba.misindicadores.ui.indicators
+
+import androidx.lifecycle.ViewModel
+
+class IndicatorDetailViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
